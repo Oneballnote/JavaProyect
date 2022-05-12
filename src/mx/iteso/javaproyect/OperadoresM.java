b9b8f31a2d55144ecf5c07ccc2faf9ecd07067a2
Cristian8a <89211137+Cristian8a@users.noreply.github.com>
2022-05-12 09:00:05 -0500

    Update OperadoresM.java

--- src/mx/iteso/javaproyect/OperadoresM.java
+++ src/mx/iteso/javaproyect/OperadoresM.java
@@ -50,6 +50,47 @@ public class OperadoresM implements operatorsM{
 	}
 
 	@Override
+	public boolean baseCanonica(double[][] lolo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[][] MultipM(double[][] r) {
+		double[][] c = new double[this.MR.length][r[0].length];
+	    // se comprueba si las matrices se pueden multiplicar
+	    if (this.MR[0].length == r.length) {
+	        for (int i = 0; i < this.MR.length; i++) {
+	            for (int j = 0; j < r[0].length; j++) {
+	                for (int k = 0; k < this.MR[0].length; k++) {
+	                    // aquí se multiplica la matriz
+	                    c[i][j] += this.MR[i][k] * r[k][j];
+	                }
+	            }
+	        }
+	    }
+	     //si no se cumple la condición se retorna una matriz vacía
+	    return c;
+	}
+
+}
+		}				
+		return MR;
+	}
+
+	@Override
+	public double[][] productoxEscalar(int z) {
+		for(int i = 0; i < MR.length; i++)
+		{
+			for(int j = 0; j < MR.length; j++)
+			{
+				this.MR[i][j] *= z;
+			}
+		}
+		return this.MR;
+	}
+
+	@Override
 	public boolean independenciaLineal() {
 		// TODO Auto-generated method stub
 		return false;
