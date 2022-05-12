fb4efe7441679f9a5db3905c0c8735a71ed85c14
Cristian8a <89211137+Cristian8a@users.noreply.github.com>
2022-05-12 08:33:47 -0500

    Delete operatorsM.java

--- src/mx/iteso/javaproyect/operatorsM.java
+++
@@ -1,9 +0,0 @@
-package mx.iteso.javaproyect;
-
-public interface OperatorsM {
-
-	public double[][] productoxEscalar(int y);
-	public abstract double [][] suma(double[][] x);
-	public abstract double [][] resta(double[][] y);	
-	
-}
