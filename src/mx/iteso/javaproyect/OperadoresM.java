8eb55610ea7bc00efbd8d71fb2b3dcfadf35b575
Cristian8a <89211137+Cristian8a@users.noreply.github.com>
2022-05-12 08:27:08 -0500

    Add files via upload

--- src/mx/iteso/javaproyect/OperadoresM.java
+++ src/mx/iteso/javaproyect/OperadoresM.java
@@ -1,6 +1,6 @@
 package mx.iteso.javaproyect;
 
-public class OperadoresM implements OperatorsM{
+public class OperadoresM implements operatorsM{
     protected double MR[][]; 
 	
 	public OperadoresM(double[][] M1){
@@ -37,12 +37,65 @@ public class OperadoresM implements OperatorsM{
 		return MR;
 	}
 
-    public double[][] productoxEscalar(int y){
-        for (int i = 0; i < MR.length; i++) {
-            for (int j = 0; j < MR.length; j++) {
-                this.MR[i][j] *= y;
-            }
-        }
-        return this.MR;
-    }
+	@Override
+	public double[][] productoxEscalar(int z) {
+		for(int i = 0; i < MR.length; i++)
+		{
+			for(int j = 0; j < MR.length; j++)
+			{
+				this.MR[i][j] *= z;
+			}
+		}
+		return this.MR;
+	}
+
+	@Override
+	public boolean independenciaLineal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean baseCanonica(double[][] lolo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void obtenerCofactor(int[][] M, int[][] temp, int p, int q, int n) {
+		int i = 0; int j = 0;
+		for(int fila = 0; fila < n; fila++)
+		{
+			for(int columna = 0; columna < n; columna++)
+			{
+				if(fila != p && columna != q)
+				{
+					temp[i][j++] = M[fila][columna];
+					if(j == n - 1)
+					{
+						j = 0;
+						i++;
+					}
+				}
+			}
+		}
+	}
+	
+	public int determinante(int M[][], int n) {
+		int det = 0;
+		if(n == 1) 
+		{
+			return M[0][0];
+		}
+		int temp[][] = new int[n][n];
+		int multp = 1;
+		for(int f = 0; f < n; f++)
+		{
+			obtenerCofactor(M, temp, 0, f, n);
+			det += multp * M[0][f] * determinante(temp, n-1);
+			multp = -multp;
+		}
+		return det;
+		
+	}
 }
