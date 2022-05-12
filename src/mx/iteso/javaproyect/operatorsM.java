5de21dbeb4f3f0a3f94b5cb10444ec987c85fc03
Cristian8a <89211137+Cristian8a@users.noreply.github.com>
2022-05-12 09:02:22 -0500

    Update operatorsM.java

--- src/mx/iteso/javaproyect/operatorsM.java
+++ src/mx/iteso/javaproyect/operatorsM.java
@@ -3,9 +3,9 @@ package mx.iteso.javaproyect;
 public interface operatorsM {
 
 	public abstract double[][] productoxEscalar(int z);
-	public abstract boolean independenciaLineal();
 	public abstract boolean baseCanonica(double[][] lolo);
 	public abstract double [][] suma(double[][] x);
 	public abstract double [][] resta(double[][] y);
+	double[][] MultipM(double[][] r);
 		
 }
